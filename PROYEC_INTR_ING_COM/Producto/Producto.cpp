d191aa6c69647709a54a7cc34a44bd68db074302
ValeriaSG <valeriasantiago319@aragon.unam.mx>
2021-12-20 23:35:15 -0600

    Anexo del Producto

---
+++ PROYEC_INTR_ING_COM/Producto/Producto.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+float Nm1, Nm2, Prod;
+
+scanf("%f", Nm1);
+scanf("%f", Nm2);
+
+Prod= Nm1 * Nm2;
+
+printf ("%f", Prod);
+return 0;
+}
+
+
